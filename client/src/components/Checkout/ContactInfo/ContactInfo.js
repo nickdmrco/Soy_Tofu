cfdf8faedfbdeaa786008e5d95952dc8d9220b03
CeliaNoriega <noriegacelia2124@gmail.com>
2021-01-11 10:39:10 -0800

    checkout layout

---
+++ client/src/components/Checkout/ContactInfo/ContactInfo.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+
+export default function ContactInfo() {
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Contact Info
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="firstName"
+            name="firstName"
+            label="First name"
+            fullWidth
+            autoComplete="given-name"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="lastName"
+            name="lastName"
+            label="Last name"
+            fullWidth
+            autoComplete="family-name"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="email"
+            name="email"
+            label="Email"
+            fullWidth
+            autoComplete="email"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="phone"
+            name="phone"
+            label="Phone"
+            fullWidth
+            autoComplete="phone"
+          />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
