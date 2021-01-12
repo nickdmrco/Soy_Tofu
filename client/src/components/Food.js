3ff6a1990a4b1b3ee8537ea4918c83979f2d1162
CeliaNoriega <noriegacelia2124@gmail.com>
2021-01-12 14:59:43 -0800

    removed unnecessary imports from old footer

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container'
 import Footer from './Footer'
 
 const useStyles = makeStyles((theme) => ({
