c6e50610c430841f864b84cca4d372b26f6db623
CeliaNoriega <noriegacelia2124@gmail.com>
2021-01-12 14:59:16 -0800

    changed footer

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -3,20 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
-import Link from '@material-ui/core/Link'
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  )
-}
+import Footer from './Footer'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,13 +13,8 @@ const useStyles = makeStyles((theme) => ({
   },
   main: {
     marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2),
-  },
-  footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',
-    backgroundColor: "blue"
-  },
+    marginBottom: theme.spacing(2)
+  }
 }))
 
 const Food = () => {
@@ -40,25 +22,8 @@ const Food = () => {
 
   return (
     <div className={classes.root}>
-      <CssBaseline />
-      <Container component="main" className={classes.main} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          Sticky footer
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          {'Pin a footer to the bottom of the viewport.'}
-          {'The footer will move as the main element of the page grows.'}
-        </Typography>
-        <Typography variant="body1">Sticky footer placeholder.</Typography>
-      </Container>
-      <footer className={classes.footer}>
-        <Container maxWidth="sm">
-          <Typography variant="body1">
-            My sticky footer can be found here.
-          </Typography>
-          <Copyright />
-        </Container>
-      </footer>
+  
+      <Footer></Footer>
     </div>
   )
 }
