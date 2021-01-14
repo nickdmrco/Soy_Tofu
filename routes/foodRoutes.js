72ab745653e7d8fa3c92fdc80deed6bd5636d6bd
Richard Shaw <richardmorganshaw@gmail.com>
2021-01-14 10:54:27 -0800

    created routes

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,10 +1,28 @@
 const router = require('express').Router()
 const { Food } = require('../models')
 
-router.get('/food',(req,res)=>{
+router.get('/food', (req, res) => {
   Food.Find()
-  .then(food => res.json(food))
-  .catch(err => console.log(err))
+    .then((food) => res.json(food))
+    .catch((err) => console.log(err))
 })
 
-module.exports = router;
+router.post('/food', (req, res) => {
+  Food.create(req.body)
+    .then((food) => res.json(food))
+    .catch((err) => console.log(err))
+})
+
+router.put('/food/:id',(req,res)=>{
+  Food.findByIdAndUpdate(req.params.id,{$set:req.body})
+  .then(()=? res.sendStatus(200))
+  .catch(err=>console.log(err))
+})
+
+router.delete('/food/:id', (req, res) => {
+  Food.findByIdAndDelete(req.params.id)
+    .then(() => res.sendStatus(200))
+    .catch((err) => console.log(err))
+})
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
 
 router.use('/api',require('./foodRoutes.js'))
+
 module.exports = router
