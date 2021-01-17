8c710159114e152051b75a06afd01520447eccba
CeliaNoriega <noriegacelia2124@gmail.com>
2021-01-17 15:30:45 -0800

    functional appBar

---
+++ celia/models/Food.js
@@ -0,0 +1,13 @@
+const { model, Schema } = require('mongoose')
+
+module.exports = model(
+  'Food',
+  new Schema({
+    name: String,
+    image: String,
+    price: Number,
+    catagory: String,
+    description: String,
+    options: Array,
+  }),
+)
