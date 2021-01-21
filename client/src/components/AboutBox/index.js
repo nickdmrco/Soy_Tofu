40416faeee4f95baa3eae35c4a193a2f694a9324
nickdmrco@gmail.com <nickdmrco@gmail.com>
2021-01-21 11:57:57 -0800

    Update to MAIN

---
+++ client/src/components/AboutBox/index.js
@@ -0,0 +1,54 @@
+// import 'fontsource-roboto'
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Parallax } from 'react-parallax'
+import Typography from '@material-ui/core/Typography'
+import full from '../../img/bibimbap.jpg'
+import './style.css'
+
+const useStyles = makeStyles((theme) => ({
+ body: {
+  fontFamily: 'Roboto'
+ },
+ typography: {
+  marginTop: '3rem',
+  marginBottom: '3rem',
+ },
+ center: {
+  marginLeft: '2%',
+  marginRight: '2%',
+  fontSize: '100%',
+  lineHeight: '200%'
+ },
+ inside: {
+  background: '#fff',
+  top: '50%',
+  left: '50%',
+  position: 'absolute',
+  padding: '20px',
+  transform: 'translate(-50%, -50%)'
+ }
+}))
+
+const AboutBox = () => {
+ const classes = useStyles()
+ return (
+  <div className={classes.body}>
+   <Parallax bgImage={full} strength={400}>
+    <div style={{ height: 650 }}>
+     <div className={classes.inside + ' blue'}>
+      <Typography className={classes.typography} variant="h3" align="center">
+       About Us
+      </Typography>
+      <p className={classes.center}>
+       Soy Tofu specializes in Korean tofu stew and Korean BBQ. We use only the freshest and highest quality ingredients in our dishes, as well as preparing them daily to guarantee the best experience. Our side dishes (banchan) are always changing as we try our best to use ingredients that are in season. We treat our food as an art, and you will experience this first hand when you come through our doors. We humbly welcome you as our guests and await your next visit to our restaurant!
+      </p>
+     </div>
+    </div>
+   </Parallax>
+  </div>
+ )
+}
+
+
+export default AboutBox
---
+++ client/src/utils/TileData/index.js
@@ -0,0 +1,25 @@
+import Soup from '../../img/bibimbap.jpg'
+
+const tileData = [
+ {
+  img: Soup,
+  title: 'Soup',
+  author: 'author',
+ },
+ {
+  img: Soup,
+  title: 'Image',
+  author: 'author',
+ },
+ {
+  img: Soup,
+  title: 'Image',
+  author: 'author',
+ },
+ {
+  img: Soup,
+  title: 'Image',
+  author: 'author',
+ }
+  ];
+  export default tileData
