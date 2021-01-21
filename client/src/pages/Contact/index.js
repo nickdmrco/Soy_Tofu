dee3fdf101c681f2b2b33d975ebb6c6bf61b7f7e
Loreina Salalila <vialoraine.gg@gmail.com>
2021-01-21 13:39:36 -0800

    email form - formspree.io

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../../components/Footer'
 import Navbar from '../../components/Navbar'
+import MyForm from "../../components/ContactForm/myform"
 
 const Contact = () => {
  return (
   <>
    <Navbar />
+   <MyForm />
     THIS IS THE CONTACT PAGE.
    <Footer />
   </>
